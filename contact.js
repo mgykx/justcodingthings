68eb19ffb20f3751ad7018a214a6e0abe6aea562
mgykx <megankim1288@gmail.com>
2019-10-22 19:39:11 -0700

    contactjs commit

---
+++ contact.js
@@ -0,0 +1,17 @@
+var vara = new Vara("#container", "https://rawcdn.githack.com/akzhy/Vara/ed6ab92fdf196596266ae76867c415fa659eb348/fonts/Satisfy/SatisfySL.json", [{
+  text: "meetings",
+  fontSize: 48,
+  y:10,
+  color:"#d9d5de"
+}], {
+  strokeWidth: 2,
+  textAlign:"center"
+});
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "25%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
